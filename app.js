d4387ee9c530da71a05db29617d964f0babd78ae
MonoChromeCN <103027281+MonoChromeCN@users.noreply.github.com>
2023-10-30 16:18:35 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { TextDecoder } = require('util');
 const logcb= (...args)=>console.log.bind(this,...args);
 const errcb= (...args)=>console.error.bind(this,...args);
 
-const uuid= (process.env.UUID||'d342d11e-d424-4583-b36e-524ab1f0afa4').replace(/-/g, "");
+const uuid= (process.env.UUID||'8b8706b6-aaf4-40c5-b60d-b70924a4b18a').replace(/-/g, "");
 const port= process.env.PORT||3000;
 
 const wss=new WebSocket.Server({port},logcb('listen:', port));
